feffd6e2aba062fb93487cd6c4289d31cd3e8a7e
shreya kajbaje <51292172+shreyakajbaje@users.noreply.github.com>
2024-10-25 19:27:48 +0530

    adding template

---
+++ template.py
@@ -0,0 +1,20 @@
+# Topic(Arrays/Strings): Problem Title with Problem number e.g. String:101 Problem Title
+
+# Problem Pattern - Sliding Window, Two Pointers, Hare & Tortoise algorithm, Merge Intervals, Subsets, Top K elements,
+# refer - https://leetcode.com/discuss/study-guide/4039411/14-Patterns-to-Ace-Any-Coding-Interview-Question
+
+# Example
+
+# Solution 1
+
+#step1 
+
+#step2
+
+# Solution 2 (Optimized)
+
+#step1
+
+#step2
+
+#note/catch
